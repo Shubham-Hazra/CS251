ed93e27314d55ef207c9ca5c7dcd7bf485561684
Shubham <shubhamhazra1234@gmail.com>
2022-08-17 11:39:00 +0530

    SSL LAB

---
+++ 210100143_inlab1/lab1.js
@@ -0,0 +1,18 @@
+function changecolorblue()
+{
+    var element=document.getElementById('para');
+    element.style.color="blue"
+}
+
+function changecolorred()
+{
+    var element=document.getElementById('para');
+    element.style.color="red"
+}
+
+function show_alert()
+{
+    var name = document.getElementById('name').value;
+    alert("Hello "+name+ ", You're doing well!")
+}
+
